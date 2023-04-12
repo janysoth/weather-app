eafc85fbd99e34b0d58693d69df6455e235cf83c
Jonny Soth <janysoth@gmail.com>
2023-04-12 00:01:55 -0500

    Create useEffect to get user current location.

--- src/weather-app/src/App.js
+++ src/weather-app/src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import { Dimmer, Loader } from 'semantic-ui-react';
+import Weather from './components/weather';
+//import Forecast from './components/forecast';
 
-function App() {
+export default function App() {
+
+  // Create 2 states: latitude & longitude.
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  // To load the functions when the app is loaded & reloaded.
+  useEffect(() => {
+    // To get current location:
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat)
+    console.log("Longitude is:", long)
+  }, [lat, long]);
+
   return (
     <div className="App">
 
     </div>
   );
 }
-
-export default App;
