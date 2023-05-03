9b1026e3bd621a879c2e18fd628f563af072579a
Jonny Soth <janysoth@gmail.com>
2023-05-03 03:00:20 -0500

    Try to loop through the array but it doesn't work.

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,12 @@ import { getUsers } from "../api/weatherApi.js";
 const getComments = async () => {
   const data = await getUsers;
   console.log(data);
-  data.forEach((user) => {
+  for (i = 0; i < data.length; i++) {
     const comment = `<li>${user.body}</li>`;
-    console.log(comment);
-
     document.querySelector("ul").insertAdjacentHTML("beforeend", comment);
-  });
+  }
 };
 
 getComments();
+
+//document.querySelector("ul").insertAdjacentHTML("beforeend", comment);
