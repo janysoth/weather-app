88617707b63247a3a39d2bc2b5c0276b4a2235bf
Jonny Soth <janysoth@gmail.com>
2023-05-24 22:32:25 -0500

    Move the api function to weatherApi.js

--- api/weatherApi.js
+++ api/weatherApi.js
@@ -1,15 +1,27 @@
 // api url
-const url = "https://jsonplaceholder.typicode.com/posts/1/comments";
+const apiKey = "d5d4f9caa31dc0bc654d876b1d629d6e";
 
-export const getUsers = async () => {
-  const response = await fetch(url);
+// export const getUsers = async () => {
+//   const response = await fetch(url);
+//   const data = await response.json();
+
+//   return data;
+// };
+
+export const getWeatherData = async (city) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  );
   const data = await response.json();
 
   return data;
 };
 
-
-
-
-
-
+// Function to get daily forecast data from API
+export const getForecastData = async (city) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+  );
+  const data = await response.json();
+  return data;
+};
